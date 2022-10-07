0f3f33e2a713e96c73ae9476f9e24705b725ad79
Da-zeros <daniromanpt@gmail.com>
2022-10-08 00:06:09 +0200

    distance method

--- src/App.java
+++ src/App.java
@@ -3,10 +3,13 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Point point1 = new Point(10.0, 20.0);
-		Cercle cercle1 = new Cercle(point1, 20.0);
+		Point point1 = new Point(20.0, 20.0);
+		Point point2 = new Point(20.0, 20.0);
 		
-		System.out.println(cercle1.showAllInfo(cercle1)); 
+		Cercle cercle1 = new Cercle(point1, 20.0);
+		Cercle cercle2 = new Cercle(point2, 20.0);
+		
+		System.out.println(cercle1.showAllInfo(cercle2)); 
 	}
 	
 	
